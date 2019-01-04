b29492e17f60d8747cd9096de02ed6c319326f8f
wuqihan <wuqh_ios@icloud.com>
2019-01-04 08:57:18 +0800

    init

---
+++ DDDingDingClock/DDDingDingClock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DDDingDingClock
+//
+//  Created by wuqh on 2019/1/4.
+//  Copyright © 2019 wuqh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
